329abd01b93d8e945b43a1ce0f55c5a3e6df654e
Paulo Eduardo <pauloeduardo.dg@outlook.com>
2021-11-27 23:19:18 -0300

    Teste Cliente Domain

--- backend/src/main/java/net/atos/api/cliente/service/CadastrarCliente.java
+++ backend/src/main/java/net/atos/api/cliente/service/CadastrarCliente.java
@@ -19,6 +19,18 @@ public class CadastrarCliente {
 				.orElseThrow(() -> new BadRequestException("Campo: DataAlteracao não pode ser nulo"));
 		Optional.ofNullable(cliente.getStatus())
 				.orElseThrow(() -> new BadRequestException("status do cliente não pode ser nulo"));
+		Optional.ofNullable(cliente.getNome())
+				.orElseThrow(() -> new BadRequestException("nome não pode ser nulo"));
+		Optional.ofNullable(cliente.getCpf())
+				.orElseThrow(() -> new BadRequestException("cpf não pode ser nulo"));
+		Optional.ofNullable(cliente.getEmail())
+				.orElseThrow(() -> new BadRequestException("email não pode ser nulo"));
+		Optional.ofNullable(cliente.getTelefone())
+				.orElseThrow(() -> new BadRequestException("telefone não pode ser nulo"));
+		Optional.ofNullable(cliente.getCelular())
+				.orElseThrow(() -> new BadRequestException("celular não pode ser nulo"));
+		Optional.ofNullable(cliente.getNascimento())
+				.orElseThrow(() -> new BadRequestException("nascimento não pode ser nulo"));
 
 	}
 	
--- backend/src/test/java/net/atos/api/cliente/service/CadastrarClienteTest.java
+++ backend/src/test/java/net/atos/api/cliente/service/CadastrarClienteTest.java
@@ -16,6 +16,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import net.atos.api.cliente.domain.Cliente;
+import net.atos.api.cliente.domain.Status;
 
 @ExtendWith(MockitoExtension.class)
 class CadastrarClienteTest {
@@ -68,7 +69,116 @@ class CadastrarClienteTest {
 		
 		assertEquals("status do cliente não pode ser nulo", assertThrows.getMessage());
 	}
-
 	
+	@Test
+	@DisplayName("Teste do campo: nome")
+	public void nome_null() {
+		
+		assertNotNull(cadastrarCliente);
+		
+		Cliente cliente = new Cliente();
+		cliente.setDataCadastro(LocalDate.now());
+		cliente.setDataAlteracao(LocalDateTime.now());
+		cliente.setStatus(Status.INATIVO);
+		
+		var assertThrows = assertThrows(BadRequestException.class, () -> cadastrarCliente.persistir(cliente));
+		
+		assertEquals("nome não pode ser nulo", assertThrows.getMessage());
+	}
+	
+	@Test
+	@DisplayName("Teste do campo: cpf")
+	public void cpf_null() {
+		
+		assertNotNull(cadastrarCliente);
+		
+		Cliente cliente = new Cliente();
+		cliente.setDataCadastro(LocalDate.now());
+		cliente.setDataAlteracao(LocalDateTime.now());
+		cliente.setStatus(Status.INATIVO);
+		cliente.setNome("Nome do cliente");
+		
+		var assertThrows = assertThrows(BadRequestException.class, () -> cadastrarCliente.persistir(cliente));
+		
+		assertEquals("cpf não pode ser nulo", assertThrows.getMessage());
+	}
+
+	@Test
+	@DisplayName("Teste do campo: email")
+	public void email_null() {
+		
+		assertNotNull(cadastrarCliente);
+		
+		Cliente cliente = new Cliente();
+		cliente.setDataCadastro(LocalDate.now());
+		cliente.setDataAlteracao(LocalDateTime.now());
+		cliente.setStatus(Status.INATIVO);
+		cliente.setNome("Nome do cliente");
+		cliente.setCpf("000.000.000-99");
+		
+		var assertThrows = assertThrows(BadRequestException.class, () -> cadastrarCliente.persistir(cliente));
+		
+		assertEquals("email não pode ser nulo", assertThrows.getMessage());
+	}
+	
+	@Test
+	@DisplayName("Teste do campo: telefone")
+	public void telefone_null() {
+		
+		assertNotNull(cadastrarCliente);
+		
+		Cliente cliente = new Cliente();
+		cliente.setDataCadastro(LocalDate.now());
+		cliente.setDataAlteracao(LocalDateTime.now());
+		cliente.setStatus(Status.INATIVO);
+		cliente.setNome("Nome do cliente");
+		cliente.setCpf("000.000.000-99");
+		cliente.setEmail("teste@email.com.br");
+		
+		var assertThrows = assertThrows(BadRequestException.class, () -> cadastrarCliente.persistir(cliente));
+		
+		assertEquals("telefone não pode ser nulo", assertThrows.getMessage());
+	}
+	
+	@Test
+	@DisplayName("Teste do campo: celular")
+	public void celular_null() {
+		
+		assertNotNull(cadastrarCliente);
+		
+		Cliente cliente = new Cliente();
+		cliente.setDataCadastro(LocalDate.now());
+		cliente.setDataAlteracao(LocalDateTime.now());
+		cliente.setStatus(Status.INATIVO);
+		cliente.setNome("Nome do cliente");
+		cliente.setCpf("000.000.000-99");
+		cliente.setEmail("teste@email.com.br");
+		cliente.setTelefone("(99) 9999-9999");
+		
+		var assertThrows = assertThrows(BadRequestException.class, () -> cadastrarCliente.persistir(cliente));
+		
+		assertEquals("celular não pode ser nulo", assertThrows.getMessage());
+	}
+	
+	@Test
+	@DisplayName("Teste do campo: nascimento")
+	public void nascimento_null() {
+		
+		assertNotNull(cadastrarCliente);
+		
+		Cliente cliente = new Cliente();
+		cliente.setDataCadastro(LocalDate.now());
+		cliente.setDataAlteracao(LocalDateTime.now());
+		cliente.setStatus(Status.INATIVO);
+		cliente.setNome("Nome do cliente");
+		cliente.setCpf("000.000.000-99");
+		cliente.setEmail("teste@email.com.br");
+		cliente.setTelefone("(99) 9999-9999");
+		cliente.setCelular("(99) 9 9999-9999");
+		
+		var assertThrows = assertThrows(BadRequestException.class, () -> cadastrarCliente.persistir(cliente));
+		
+		assertEquals("nascimento não pode ser nulo", assertThrows.getMessage());
+	}
 	
 }
